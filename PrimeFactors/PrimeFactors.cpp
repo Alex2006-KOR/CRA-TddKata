081373f4171c8fb23729e02f5bb9ce3666bff275
ys88.lee <ys88.lee@gmail.com>
2024-05-30 13:30:20 +0900

    [refatoring] 41

--- PrimeFactors/PrimeFactors.cpp
+++ PrimeFactors/PrimeFactors.cpp
@@ -4,14 +4,10 @@ using std::vector;
 
 class PrimeFactor {
 public:
-	vector<int> of(int i) {
+	vector<int> of(int num) {
 		vector<int> result = {};
-		if (i == 2) {
-			result.push_back(2);
-		}
-		if (i == 3) {
-			result.push_back(3);
-		}
+		if (num > 1)
+			result.push_back(num);
 		return result;
 	}
 };
