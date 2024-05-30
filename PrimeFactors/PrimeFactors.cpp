df3b6884f921d18022a612c8e474b2100986c898
ys88.lee <ys88.lee@gmail.com>
2024-05-30 13:42:13 +0900

    [refactoring] 59

--- PrimeFactors/PrimeFactors.cpp
+++ PrimeFactors/PrimeFactors.cpp
@@ -6,21 +6,12 @@ class PrimeFactor {
 public:
 	vector<int> of(int num) {
 		vector<int> result = {};
-		if (num > 1) {
-			int divisor = 2;
-			if (num == 4 || num == 6 || num == 9 || num == 12) {
-				for (divisor = 2; num > 1; divisor++) {
-					while (num % divisor == 0) {
-						result.push_back(divisor);
-						num /= divisor;
-					}
-				}
+		for (int divisor = 2; num > 1; divisor++) {
+			while (num % divisor == 0) {
+				result.push_back(divisor);
+				num /= divisor;
 			}
-			else {
-				result.push_back(num);
-			}
-		}
-			
+		}			
 		return result;
 	}
 };
