336e320352fc1afa491667a197b10e90c80a83d1
ys88.lee <ys88.lee@gmail.com>
2024-05-30 14:54:48 +0900

    [refactoring] 28

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& string) {
-		throw length_error("Must be three letters.");
+		if(string.length() != 3)
+			throw length_error("Must be three letters.");
 	}
 };
