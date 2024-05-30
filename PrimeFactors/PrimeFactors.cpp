5b589f5894fcc73511b76bd6197b0ac69e17285f
ys88.lee <ys88.lee@gmail.com>
2024-05-30 13:34:12 +0900

    [refactoring] 46

--- PrimeFactors/PrimeFactors.cpp
+++ PrimeFactors/PrimeFactors.cpp
@@ -8,8 +8,10 @@ public:
 		vector<int> result = {};
 		if (num > 1) {
 			if (num == 4) {
-				result.push_back(2);
-				result.push_back(2);
+				while (num % 2 == 0) {
+					result.push_back(2);
+					num /= 2;
+				}
 			}
 			else {
 				result.push_back(num);
