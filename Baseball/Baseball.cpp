b6f284f5f337c09bc9e38ebf3152cd62ed6e5bcb
ys88.lee <ys88.lee@gmail.com>
2024-05-30 15:19:22 +0900

    [refactoring] 50

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -15,7 +15,10 @@ public:
 
 	GuessResult guess(const string& guessNumber) {
 		assertIllegalArgument(guessNumber);
-		return { true, 3, 0 };
+
+		if(guessNumber == question)
+			return { true, 3, 0 };
+		return { false, 0, 0 };
 	}
 
 private:
