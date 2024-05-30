ce3aaee01e9617b0b8136bed3cedd503870d11d3
ys88.lee <ys88.lee@gmail.com>
2024-05-30 11:58:58 +0900

    [feature] 37

--- PrimeFactors-Test/PrimeFactors_test.cpp
+++ PrimeFactors-Test/PrimeFactors_test.cpp
@@ -3,8 +3,14 @@
 
 using namespace std;
 
-TEST(PrimeFactors, PrimeTest) {
+TEST(PrimeFactors, Of1) {
 	PrimeFactor primeFactor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, primeFactor.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor primeFactor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, primeFactor.of(2));
 }
--- PrimeFactors/PrimeFactors.cpp
+++ PrimeFactors/PrimeFactors.cpp
@@ -6,6 +6,9 @@ class PrimeFactor {
 public:
 	vector<int> of(int i) {
 		vector<int> result = {};
+		if (i == 2) {
+			result.push_back(2);
+		}
 		return result;
 	}
 };
