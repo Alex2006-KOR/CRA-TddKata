b525c818e2caf1f9ff927a0a6170261ef98392c6
ys88.lee <ys88.lee@gmail.com>
2024-05-31 11:07:14 +0900

    [feature/refatoring] get length score

--- SimilarityChecker-Test/SimilarityChecker_test.cpp
+++ SimilarityChecker-Test/SimilarityChecker_test.cpp
@@ -1,8 +1,29 @@
 #include "pch.h"
 #include "SimilarityChecker.cpp"
 
-TEST(SimilarityCheckerTest, GetStringLengthMaxScore) {
-	SimilarityChecker checker;
-	int expected = 60;
-	EXPECT_EQ(expected, checker.getSimilarityScore("ABC", "ABC"));
+#include <vector>
+
+using std::vector;
+
+struct TestVector_t {
+	int expected;
+	string srcStr;
+	string dstStr;
+};
+
+class SimilarityCheckerTestFixture : public SimilarityChecker, public testing::Test {
+public:
+	void doLengthTest(vector<TestVector_t> tv) {
+		for (auto& v : tv)
+			EXPECT_EQ(v.expected, getLengthScore(v.srcStr, v.dstStr));
+	}
+};
+
+TEST_F(SimilarityCheckerTestFixture, DoLengthScoreTest) {
+	vector<TestVector_t> tv = {
+		{60, "ABC", "DEF"},
+		{30, "ABCDEF", "GHIK"},
+		{0, "ABCDEF", "GHI"}
+	};
+	doLengthTest(tv);
 }
