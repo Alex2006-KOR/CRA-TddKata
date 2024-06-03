1533b8efd7fe40fd4ab6137b36bcc51faab72cb3
DESKTOP-5FBE16T\User <user@user.com>
2024-06-03 09:32:40 +0900

    [feature] test project init

--- DeviceDriver-Test/DeviceDriver_test.cpp
+++ DeviceDriver-Test/DeviceDriver_test.cpp
@@ -1,7 +1,35 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "DeviceDriver.h"
+
+using namespace testing;
+using namespace std;
+
+class FlashMemoryDeviceMock : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+class DeviceDriverTestFixture : public testing::Test {
+public:
+	FlashMemoryDeviceMock flashDeviceMock;
+	DeviceDriver* pDeviceDriver;
+
+protected:
+	// 초기화
+	virtual void SetUp() {
+		std::cout << "\t\DeviceDriverTestFixture - SetUp" << std::endl;
+		pDeviceDriver = new DeviceDriver(&flashDeviceMock);
+	}
+
+	// 정리
+	virtual void TearDown() {
+		std::cout << "\t\DeviceDriverTestFixture - TearDown" << std::endl;
+		delete pDeviceDriver;
+	}
+};
+
+TEST(DeviceDriverTest, Read) {
 }
